86dc8159c19fb95e4558be2dd1fc01b2f0518ca3
Andrew <ng9417265@gmail.com>
2023-02-19 21:49:32 -0500

    stuff

--- app/src/main/java/map/editor/Map.java
+++ app/src/main/java/map/editor/Map.java
@@ -23,6 +23,7 @@ import javafx.event.EventHandler;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
@@ -31,6 +32,12 @@ import javafx.scene.text.TextAlignment;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.control.ToolBar;
+import javafx.scene.control.Separator;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ColorPicker;
+
 
 import map.editor.MapTab;
 
@@ -44,15 +51,20 @@ public class Map{
 
     private ScrollPane scrollPane;
     private GridPane gridPane;
-    private AnchorPane buttonPane;
     private int size = 20;
 
     private int width;
     private int height;
     private Tab tab;
-    private Button plusButton;
-    private Button minusButton;
-    private Button fitButton;
+
+    private ColorPicker colorPicker;
+    private int currentId;
+    private Tool currentTool;
+
+    public enum Tool {
+        BRUSH,
+        ERASER
+    }
 
     public Map(int width, int height) {
         // Initialize map and UI elements
@@ -60,23 +72,7 @@ public class Map{
         this.height = height;
         this.map = new int[width][height];
         this.tab = new MapTab("Untitled", this);
-
-        this.gridPane = new GridPane();
-        this.scrollPane = new ScrollPane();
-
-        BorderPane borderPane = new BorderPane();
-        this.tab.setContent(borderPane);
-        
-        createButtons();
-        redraw();
-
-        // Add scrolling
-        this.scrollPane.setContent(gridPane);
-        this.scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        this.scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        borderPane.setCenter(scrollPane);
-        borderPane.setBottom(toolbar);
+        createMap();
     }
 
     public Map(int[][] array, File file)
@@ -87,38 +83,110 @@ public class Map{
         this.map = array;
         this.tab = new MapTab(file.getName(),this);
         setFile(file);
+        createMap();
+    }
 
+    public void createMap() {
         this.gridPane = new GridPane();
         this.scrollPane = new ScrollPane();
 
         BorderPane borderPane = new BorderPane();
         this.tab.setContent(borderPane);
 
-        createButtons();
-        redraw();
-        
-        // Add scrolling
         this.scrollPane.setContent(gridPane);
         this.scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         this.scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
         borderPane.setCenter(scrollPane);
-        createButtonPane();
-        borderPane.setBottom(getButtonPane());
+
+        // Create the toolbar and zoom buttons
+        toolbar = new ToolBar();
+        Button zoomInButton = new Button("+");
+        Button zoomOutButton = new Button("-");
+        Button fitButton = new Button("=");
+        
+        TextField idBox = new TextField();
+        idBox.setPrefWidth(25);
+        idBox.setText("1");
+        currentId = 1;
+        idBox.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.matches("\\d+")) {
+                currentId = Integer.parseInt(newValue);
+            }
+        });
+
+        colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.BLACK);
+
+        Button brush = new Button("Brush");
+        Button eraser = new Button("Eraser");
+
+        Separator separator = new Separator(Orientation.VERTICAL);
+
+        redraw();
+
+        // Add the zoom buttons to the toolbar
+        toolbar.getItems().addAll(idBox,colorPicker,brush,eraser,separator,zoomInButton, zoomOutButton, fitButton);
+
+        // Add the toolbar to the bottom of the border pane
+        borderPane.setBottom(toolbar);
+
+        this.currentTool = Tool.BRUSH;
+        brush.setOnAction(event -> {currentTool = Tool.BRUSH;});
+        eraser.setOnAction(event -> {currentTool = Tool.ERASER;});
+
+        // Set the action for the zoom in button
+        zoomInButton.setOnAction(event -> {zoom(5);});
+        zoomOutButton.setOnAction(event -> {zoom(-5);});
+        fitButton.setOnAction(event -> {fitMapToWindow();});
+
+         // Add event handler for mouse pressed and dragged events
+        gridPane.setOnMousePressed(event -> {
+            if (currentTool == Tool.BRUSH || currentTool == Tool.ERASER) {
+                handleMousePressOrDrag(event);
+            }
+        });
+
+        gridPane.setOnMouseDragged(event -> {
+            if (currentTool == Tool.BRUSH || currentTool == Tool.ERASER) {
+                handleMousePressOrDrag(event);
+            }
+        });
+    }
+        
+    private void handleMousePressOrDrag(MouseEvent event) {
+        int row = (int) (event.getY() / size);
+        int col = (int) (event.getX() / size);
+
+        // Check that row and col are within the bounds of the map array
+        if (row >= 0 && row < height && col >= 0 && col < width) {
+            int id = (currentTool == Tool.BRUSH) ? currentId : 0;
+            map[row][col] = id;
+            redrawCell(row, col);
+        }
+    }
+
+    private void redrawCell(int row, int col) {
+        Group group = (Group) gridPane.getChildren().get(row * width + col);
+        Rectangle rectangle = (Rectangle) group.getChildren().get(0);
+        if(currentTool != Tool.ERASER)
+        {   
+            rectangle.setFill(colorPicker.getValue());
+        }
+        else
+        {   
+            rectangle.setFill(Color.WHITE);
+        }
+        Text text = (Text) group.getChildren().get(1);
+        text.setText(String.valueOf(map[row][col]));
     }
     
+
     public void zoom(int increment)
     {
         // Increment/decrement size to zoom in and out
         size+=increment;
         redraw();
-    }
-
-    public void createButtons() {
-        // Zoom control buttons
-        plusButton = new Button("+");
-        minusButton = new Button("-");
-        fitButton = new Button("=");
     }
 
     public void setFile(File file)
@@ -188,49 +256,21 @@ public class Map{
             for (int col = 0; col < width; col++) {
 
                 Rectangle rectangle = new Rectangle(size, size);
-                rectangle.setStyle("-fx-fill: white; -fx-stroke: rgba(0, 0, 0, 0.5); -fx-stroke-width: 1;");
-            
+                rectangle.setStyle("-fx-stroke: rgba(0, 0, 0, 0.5); -fx-stroke-width: 1;");
+                rectangle.setFill(Color.WHITE);
+    
                 Text text = new Text(String.valueOf(map[row][col]));
                 text.setFill(Color.rgb(0, 0, 0, 0.5));
                 text.setFont(Font.font("Arial", FontWeight.BOLD, size / 2));
                 text.setX(rectangle.getX() + (size - text.getLayoutBounds().getWidth()) / 2);
                 text.setY(rectangle.getY() + (size + text.getLayoutBounds().getHeight()) / 2);
-
+    
                 Group group = new Group(rectangle, text);
                 gridPane.add(group, col, row);
             }
         }
-
-        // Add the plus and minus buttons to the bottom right corner
-        createButtons();
-        createButtonPane();
-
-        // Bind events to buttons
-        plusButton.setOnAction(event -> zoom(5));
-        minusButton.setOnAction(event -> zoom(-5));
-        fitButton.setOnAction(event -> fitMapToWindow());
     }
-
-    public void createButtonPane()
-    {
-        // Create "sticky" pane in bottom right
-        buttonPane = new AnchorPane();
-        buttonPane.setBottomAnchor(plusButton, 5.0);
-        buttonPane.setRightAnchor(plusButton, 30.0);
-
-        buttonPane.setBottomAnchor(minusButton, 5.0);
-        buttonPane.setRightAnchor(minusButton, 10.0);
-
-        buttonPane.setBottomAnchor(fitButton, 5.0);
-        buttonPane.setRightAnchor(fitButton, 55.0);
-
-        // Parent the buttons to the pane
-        buttonPane.getChildren().addAll(plusButton, minusButton, fitButton);
-    }
-
-    public AnchorPane getButtonPane() {
-        return buttonPane;
-    }
+    
 
     public void fitMapToWindow() {
         // Calculate how large to make tiles to fit
